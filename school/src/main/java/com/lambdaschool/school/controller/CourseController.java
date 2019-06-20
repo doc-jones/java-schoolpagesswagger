4a3bff620ba70f15cc133167562e44b8a06b303a
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-06-20 12:03:41 -0400

    Add swagger documentation to controller/CourseController - Finished MVP

--- school/src/main/java/com/lambdaschool/school/controller/CourseController.java
+++ school/src/main/java/com/lambdaschool/school/controller/CourseController.java
@@ -1,8 +1,11 @@
 package com.lambdaschool.school.controller;
 
 import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.ErrorDetail;
+import com.lambdaschool.school.model.Student;
 import com.lambdaschool.school.service.CourseService;
 import com.lambdaschool.school.view.CountStudentsInCourses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,17 @@ public class CourseController
     @Autowired
     private CourseService courseService;
 
+    @ApiOperation(value = "return all Courses", response = Course.class, responseContainer = "List")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "page", dataType = "integer", paramType = "query",
+                    value = "Results page you want to retrieve 0..N"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+                    value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
+                    value = "Sorting criteria in the format: property(,asc|desc). " +
+                            "Default sort order is asscending. " +
+                            "Multiple sort criteria are supported.")})
+
     @GetMapping(value = "/courses", produces = {"application/json"})
     public ResponseEntity<?> listAllCourses()
     {
@@ -24,12 +38,21 @@ public class CourseController
         return new ResponseEntity<>(myCourses, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Retrieves the number of students in each course", response = Course.class)
+    @ApiResponses(value =  {
+            @ApiResponse(code = 201, message = "Student Count Found", response = Course.class),
+            @ApiResponse(code = 404, message = "Student Count Not Found", response = ErrorDetail.class)})
+
     @GetMapping(value = "/studcount", produces = {"application/json"})
     public ResponseEntity<?> getCountStudentsInCourses()
     {
         return new ResponseEntity<>(courseService.getCountStudentsInCourse(), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Deletes a course", notes = "The deleted course id will be set in the location header", response = void.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Course Deleted Successfully", response = void.class),
+            @ApiResponse(code = 500, message = "Error deleting course", response = ErrorDetail.class)})
     @DeleteMapping("/courses/{courseid}")
     public ResponseEntity<?> deleteCourseById(@PathVariable long courseid)
     {
